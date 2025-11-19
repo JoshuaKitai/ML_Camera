b4003a43b740053e0fd473e805754e8e5e7101a0
JoshuaKitai <joshuakitaigorod@ufl.edu>
2025-11-18 20:22:30 -0500

    First commit

---
+++ main.py
@@ -0,0 +1,2 @@
+#This is where my main code will lie
+#The goal of this project is to build a camera detection software, train a model based on a criteria I want, and have the model be able to detect movements I make or people or object using OpenCV, Pytorch
