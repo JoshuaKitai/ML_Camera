8aab68c17e490dfaee0ce1c3dc2122bc0b4a413e
JoshuaKitai <joshuakitaigorod@ufl.edu>
2025-11-19 20:14:19 -0500

    Getting Started with main

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 #This is where my main code will lie
 #The goal of this project is to build a camera detection software, train a model based on a criteria I want, and have the model be able to detect movements I make or people or object using OpenCV, Pytorch
+
+import OpenCV as cv
+
+if __name__ == "__main__":
+    pass
